34ac780fcbecaad4a65e48d3a576a2e538d7d90a
Isaiah Norton <isaiah.norton@gmail.com>
2015-01-17 12:48:02 -0500

    Fix and finish macro

--- src/HackThatBase.jl
+++ src/HackThatBase.jl
@@ -14,20 +14,20 @@ end
 
 file_exprs(f) = get_exprs(readall(open(f,"r")))
 
-function gdc(expr)
+function decl_name(expr)
     head = expr.head
     args = expr.args
     
     head in [:call, :typealias, :ref] &&
                          return args[1]
     head == :type     && return args[2]
-    head == :const    && return gdc(args[1])
+    head == :const    && return decl_name(args[1])
     head == :function && return args[1].args[1]
     
     head == :(=) && if isa(args[1], Symbol)
         return args[1]
     else
-        return gdc(args[1])
+        return decl_name(args[1])
     end
     nothing
 end
@@ -36,7 +36,7 @@ function body_decls(body)
     res = []
     
     for ex in body
-        s = gdc(ex)
+        s = decl_name(ex)
         try
             s != nothing && push!(res,s)
         catch
@@ -51,35 +51,22 @@ macro whisper(wname, wpath)
     wsname = string(wname); wspath = string(wpath)
     wspath = joinpath(JULIA_HOME, "../../base", string(wspath,endswith(wspath,".jl") ? "":".jl"))
 
-    exclusions = get!(HackThatBase.exclusions, wspath, HackThatBase.get_names(wspath))
-
-    function batchimp(M, exc)
-        for n in filter(x->!(x in exc), names(eval(M), true, true))
-            eval(Expr(:import, M, n))
-        end
-    end
-
-    batchimp(:Base, [:__init__, :eval, exclusions...])
-
+    exclusions = get!(HackThatBase.exclusions, wspath,
+                      [:__init__, :eval,HackThatBase.get_names(wspath)])
+   
+    importnames = setdiff(names(Base,true,true), exclusions)
     importexprs =  map(x->Expr(:import, :Base, x), setdiff(names(Base,true,true), exclusions))
 
     rexp = Expr(:toplevel, Expr(:module, false, esc(wname),
-        quote
-        eval(M,x) = Core.eval(M,x)
-        eval(x) = Core.eval($(esc(wname)), x)
-
-        #importall Core.Intrinsics
-        #importall Base.Operators
-
-        import Base: vcat, include, call, Base, names, filter,
-               !, !=, in, getindex, length, ceil, Bottom, Any, assert, zeros
-
-        $importexprs
-        include($wspath)
-
-        end)) # quote
-    # macro needs to be toplevel, and strip the surrounding block
-    #Expr(:toplevel, rexp.args[2])
+                Expr(:block,
+                    Expr(:toplevel, Expr(:import, :Base, :getindex)),
+                    Expr(:toplevel, importexprs...),
+                    esc(:(
+                        eval(M,x) = Core.eval(M,x),
+                        eval(x) = Core.eval($wname, x),
+                        include($wspath)
+                    )))
+                ))
 end
 
 end # module HackThatBase
