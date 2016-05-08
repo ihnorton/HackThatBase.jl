855e58110c895f98dc3d0f61f7767b370448c3fc
Tim Holy <tim.holy@gmail.com>
2016-05-08 06:05:57 -0500

    Restrict main tests to recent julia-0.5

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,14 @@ using Base.Test
 func(x,y) = x + y
 args = lminfo(func, (Int, Float64))
 
-#cd(joinpath(JULIA_HOME, "..", "..", "base"))
-cd(splitdir(Base.find_source_file("inference.jl"))[1])
-@hack W inference
-result = W.typeinf_uncached(args...)
-@test isa(result[1], LambdaInfo)
-@test result[2] == Float64
-@test result[3] == true
+if VERSION >= v"0.5.0-dev+3977"
+    @eval begin   # needed for julia-0.4
+        #cd(joinpath(JULIA_HOME, "..", "..", "base"))
+        cd(splitdir(Base.find_source_file("inference.jl"))[1])
+        @hack W inference
+        result = W.typeinf_uncached(args...)
+        @test isa(result[1], LambdaInfo)
+        @test result[2] == Float64
+        @test result[3] == true
+    end
+end
