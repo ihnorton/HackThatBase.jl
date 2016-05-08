733527c97c57e37fd13e9d30015116d4e6e18c25
Tim Holy <tim.holy@gmail.com>
2016-05-08 05:45:18 -0500

    Use a different strategy to find inference.jl
    Hopefully this is more Travis-friendly

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,8 @@ using Base.Test
 func(x,y) = x + y
 args = lminfo(func, (Int, Float64))
 
-cd(joinpath(JULIA_HOME, "..", "..", "base"))
+#cd(joinpath(JULIA_HOME, "..", "..", "base"))
+cd(splitdir(Base.find_source_file("inference.jl"))[1])
 @hack W inference
 result = W.typeinf_uncached(args...)
 @test isa(result[1], LambdaInfo)
